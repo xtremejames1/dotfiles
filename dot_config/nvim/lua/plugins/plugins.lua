2027f3ab633affb68d1ff765973cb6128f908f71
James Xiao <xtremejames1@gmail.com>
2024-01-13 17:28:13 -0600

    added some plugins

--- dot_config/nvim/lua/plugins/plugins.lua
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -1,46 +1,62 @@
 local plugins = {
-    {
-        "christoomey/vim-tmux-navigator",
-        lazy = false,
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+  {
+    "kylechui/nvim-surround",
+    version = "*",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end,
+  },
+  {
+    "epwalsh/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    lazy = true,
+    ft = "markdown",
+    dependencies = {
+      -- Required.
+      "nvim-lua/plenary.nvim",
     },
-    {
-        "kylechui/nvim-surround",
-        version = "*",
-        event = "VeryLazy",
-        config = function()
-            require("nvim-surround").setup({
-
-            })
-        end
-    },
-    {
-        "epwalsh/obsidian.nvim",
-        version = "*",  -- recommended, use latest release instead of latest commit
-        lazy = true,
-        ft = "markdown",
-        dependencies = {
-            -- Required.
-            "nvim-lua/plenary.nvim",
+    opts = {
+      workspaces = {
+        {
+          name = "ObsidianVault",
+          path = "/media/xtremejames1/HDD 500GB/Documents/Windows Documents 12-28-23/Obsidian Vault/",
         },
-        opts = {
-            workspaces = {
-                {
-                    name = "ObsidianVault",
-                    path = "/media/xtremejames1/HDD 500GB/Documents/Windows Documents 12-28-23/Obsidian Vault/",
-                }
-            },
-        },
-        config = function()
-            require("obsidian").setup({
-
-            })
-        end
+      },
     },
-    {
-        "karb94/neoscroll.nvim",
-        config = function ()
-            require('neoscroll').setup {}
-        end
-    }
+    config = function()
+      require("obsidian").setup({})
+    end,
+  },
+  {
+    "karb94/neoscroll.nvim",
+    config = function()
+      require("neoscroll").setup({})
+    end,
+  },
+  {
+    "stevearc/oil.nvim",
+    opts = {},
+    -- Optional dependencies
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+  },
+  {
+    "folke/todo-comments.nvim",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+  },
 }
 return plugins
